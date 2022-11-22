5521ba6cfd1ad435c831b9c8a60f0e0b862d16bd
vlanier2 <goldenlanier@gmail.com>
2022-11-22 15:02:48 -0800

    adding lab 4

---
+++ Lab 4/rb_tree.py
@@ -0,0 +1,263 @@
+class Node(object):
+    def __init__(self, data, left = None, right = None, parent = None, color = 'red'):
+        self.data = data
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.color = color
+
+
+class rb_tree(object):
+
+    PREORDER = 1
+    INORDER = 2
+    POSTORDER = 3
+    # initialize root and size
+    def __init__(self):
+        self.root = None
+        self.sentinel = Node(None, color = 'black')
+        self.sentinel.parent = self.sentinel
+        self.sentinel.left = self.sentinel
+        self.sentinel.right = self.sentinel
+    
+    def print_tree(self):
+        # Print the data of all nodes in order
+        self.__print_tree(self.root)
+    
+    def __print_tree(self, curr_node):
+        # Recursively print a subtree (in order), rooted at curr_node
+        # Printed in preorder
+        if curr_node is not self.sentinel:
+            print(str(curr_node.data), end=' ')  # save space
+            self.__print_tree(curr_node.left)
+            self.__print_tree(curr_node.right)
+
+    def __print_with_colors(self, curr_node):
+        # Recursively print a subtree (in order), rooted at curr_node
+        # Printed in PREORDER
+        # Extracts the color of the node and print it in the format -dataC- where C is B for black and R for red
+        if curr_node is not self.sentinel:
+
+            if curr_node.color is "red":
+                node_color = "R"
+            else:
+                node_color = "B"
+
+            print(str(curr_node.data)+node_color, end=' ')  # save space
+            self.__print_with_colors(curr_node.left)
+            self.__print_with_colors(curr_node.right)
+
+    def print_with_colors(self):
+        # Also prints the data of all node but with color indicators
+        self.__print_with_colors(self.root)
+            
+            
+    def __iter__(self):
+        return self.inorder()
+
+    def inorder(self):
+        return self.__traverse(self.root, rb_tree.INORDER)
+
+    def preorder(self):
+        return self.__traverse(self.root, rb_tree.PREORDER)
+
+    def postorder(self):
+        return self.__traverse(self.root, rb_tree.POSTORDER)
+
+    def __traverse(self, curr_node, traversal_type):
+        if curr_node is not self.sentinel:
+            if traversal_type == self.PREORDER:
+                yield curr_node
+            yield from self.__traverse(curr_node.left, traversal_type)
+            if traversal_type == self.INORDER:
+                yield curr_node
+            yield from self.__traverse(curr_node.right, traversal_type)
+            if traversal_type == self.POSTORDER:
+                yield curr_node
+
+    # find_min travels across the leftChild of every node, and returns the
+    # node who has no leftChild. This is the min value of a subtree
+    def find_min(self):
+        current_node = self.root
+        while current_node.left:
+            current_node = current_node.left
+        return current_node
+    
+    # find_node expects a data and returns the Node object for the given data
+    def find_node(self, data):
+        if self.root:
+            res = self.__get(data, self.root)
+            if res:
+                return res
+            else:
+                raise KeyError('Error, data not found')
+        else:
+            raise KeyError('Error, tree has no root')
+
+    # helper function __get receives a data and a node. Returns the node with
+    # the given data
+    def __get(self, data, current_node):
+        if current_node is self.sentinel: # if current_node does not exist return None
+            print("couldnt find data: {}".format(data))
+            return None
+        elif current_node.data == data:
+            return current_node
+        elif data < current_node.data:
+            # recursively call __get with data and current_node's left
+            return self.__get( data, current_node.left )
+        else: # data is greater than current_node.data
+            # recursively call __get with data and current_node's right
+            return self.__get( data, current_node.right )
+    
+
+    def find_successor(self, data):
+        # Private Method, can only be used inside of BST.
+        current_node = self.find_node(data)
+
+        if current_node is self.sentinel:
+            raise KeyError
+
+        # Travel left down the rightmost subtree
+        if current_node.right:
+            current_node = current_node.right
+            while current_node.left is not self.sentinel:
+                current_node = current_node.left
+            successor = current_node
+
+        # Travel up until the node is a left child
+        else:
+            parent = current_node.parent
+            while parent is not self.sentinel and current_node is not parent.left:
+                current_node = parent
+                parent = parent.parent
+            successor = parent
+
+        if successor:
+            return successor
+        else:
+            return None
+
+    # put adds a node to the tree
+    def insert(self, data):
+        # if the tree has a root
+        if self.root:
+            # use helper method __put to add the new node to the tree
+            new_node = self.__put(data, self.root)
+            self.__rb_insert_fixup(new_node)
+        else: # there is no root
+            # make root a Node with values passed to put
+            self.root = Node(data, parent = self.sentinel, left = self.sentinel, right = self.sentinel)
+            new_node = self.root
+            self.__rb_insert_fixup(new_node)
+    
+    #Insertion for Binary Search Tree
+    def bst_insert(self, data):
+        # if the tree has a root
+        if self.root:
+            # use helper method __put to add the new node to the tree
+            self.__put(data, self.root)
+        else: # there is no root
+            # make root a Node with values passed to put
+            self.root = Node(data, parent = self.sentinel, left = self.sentinel, right = self.sentinel)
+        
+    # helper function __put finds the appropriate place to add a node in the tree
+    def __put(self, data, current_node):
+        if data < current_node.data:
+            if current_node.left != self.sentinel:
+                new_node = self.__put(data, current_node.left)
+            else: # current_node has no child
+                new_node = Node(data,
+                parent = current_node,
+                left = self.sentinel,
+                right = self.sentinel )
+                current_node.left = new_node
+        else: # data is greater than or equal to current_node's data
+            if current_node.right != self.sentinel:
+                new_node = self.__put(data, current_node.right)
+            else: # current_node has no right child
+                new_node = Node(data,
+                parent = current_node,
+                left = self.sentinel,
+                right = self.sentinel )
+                current_node.right = new_node
+        return new_node
+
+    
+    def delete(self, data):
+        # Same as binary tree delete, except we call rb_delete fixup at the end.
+
+        z = self.find_node(data)
+        if z.left is self.sentinel or z.right is self.sentinel:
+            y = z
+        else:
+            y = self.find_successor(z.data)
+        
+        if y.left is not self.sentinel:
+            x = y.left
+        else:
+            x = y.right
+        
+        if x is not self.sentinel:
+            x.parent = y.parent
+
+        if y.parent is self.sentinel:
+            self.root = x
+
+        else:
+            if y == y.parent.left:
+                y.parent.left = x
+            else:
+                y.parent.right = x
+        
+        if y is not z:
+            z.data = y.data
+    
+        if y.color == 'black':
+            if x is self.sentinel:
+                self.__rb_delete_fixup(y)
+            else:
+                self.__rb_delete_fixup(x)
+
+    def left_rotate(self, current_node):
+        # If there is nothing to rotate with, then raise a KeyError
+        # if x is the root of the tree to rotate with left child subtree T1 and right child y, 
+        # where T2 and T3 are the left and right children of y then:
+        # x becomes left child of y and T3 as its right child of y
+        # T1 becomes left child of x and T2 becomes right child of x
+
+        # refer page 328 of CLRS book for rotations
+
+        pass
+    
+    def right_rotate(self, current_node):
+        # If there is nothing to rotate with, then raise a KeyError
+        # If y is the root of the tree to rotate with right child subtree T3 and left child x, 
+        # where T1 and T2 are the left and right children of x then:
+        # y becomes right child of x and T1 as its left child of x
+        # T2 becomes left child of y and T3 becomes right child of y
+
+        # refer page 328 of CLRS book for rotations
+
+        pass
+
+    
+    def __rb_insert_fixup(self, z):
+        # This function maintains the balancing and coloring property after bst insertion into
+        # the tree. Please red the code for insert() method to get a better understanding
+        # refer page 330 of CLRS book and lecture slides for rb_insert_fixup
+
+        pass
+
+    def __rb_delete_fixup(self, x):
+        # This function maintains the balancing and coloring property after bst deletion 
+        # from the tree. Please read the code for delete() method to get a better understanding.
+        # refer page 338 of CLRS book and lecture slides for rb_delete_fixup
+        
+        pass
+
+
+    
+
+
+    
+    
---
+++ Lab 4/test_lab4.py
@@ -0,0 +1,196 @@
+from rb_tree import Node, rb_tree
+import unittest
+ 
+class T0_tree_left_rotation(unittest.TestCase):
+
+    def test_tree_left_rotation_1(self):
+        print("\n")
+        print("tree_left_rotation")
+        tree = rb_tree()
+        tree.bst_insert(2)
+        tree.bst_insert(1)
+        tree.bst_insert(3)
+        tree.print_tree()
+        print("intial prorder tree", "\n")
+        tree.left_rotate(tree.root)
+        tree_preorder = [node.data for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [3,2,1])
+        tree.print_tree()
+        print("tree after left rotation about root  in prorder")
+        print("\n")
+    
+    def test_tree_left_rotation_2(self):
+        print("\n")
+        print("tree_left_rotation")
+        tree = rb_tree()
+        tree.bst_insert(7)
+        tree.bst_insert(5)
+        tree.bst_insert(9)
+        tree.bst_insert(3)
+        tree.bst_insert(6)
+        tree.bst_insert(8)
+        tree.bst_insert(10)
+        tree.bst_insert(1)
+        tree.bst_insert(2)
+        tree.print_tree()
+        print("intial prorder tree", "\n")
+        tree.left_rotate(tree.root)
+        tree_preorder = [node.data for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [9,7,5,3,1,2,6,8,10])
+        tree.print_tree()
+        print("tree after left rotation about root  in prorder")
+        print("\n")
+    
+
+
+class T1_tree_right_rotation(unittest.TestCase):
+
+    def test_tree_right_rotation_1(self):
+        print("\n")
+        print("tree_right_rotation")
+        tree = rb_tree()
+        tree.bst_insert(2)
+        tree.bst_insert(1)
+        tree.bst_insert(3)
+
+        tree.print_tree()
+        print("intial prorder tree", "\n")
+        tree.right_rotate(tree.root)
+        tree_preorder = [node.data for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [1,2,3])
+        tree.print_tree()
+        print("tree after right rotation about root  in prorder")
+        print("\n")
+    
+    def test_tree_right_rotation_2(self):
+        print("\n")
+        print("tree_right_rotation")
+        tree = rb_tree()
+        tree.bst_insert(7)
+        tree.bst_insert(5)
+        tree.bst_insert(9)
+        tree.bst_insert(3)
+        tree.bst_insert(6)
+        tree.bst_insert(8)
+        tree.bst_insert(10)
+        tree.bst_insert(1)
+        tree.bst_insert(2)
+        tree.print_tree()
+        print("intial prorder tree", "\n")
+        tree.right_rotate(tree.root)
+        tree_preorder = [node.data for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [5,3,1,2,7,6,9,8,10])
+        tree.print_tree()
+        print("tree after right rotation about root  in prorder")
+        print("\n")
+    
+
+class T2_tree_insert_color(unittest.TestCase):
+
+
+    def test_tree_insert_color_0(self):
+        print("\n")
+        print("tree_color_check")
+        
+        tree = rb_tree()
+
+        tree.insert(2)
+        tree.insert(1)
+        tree.insert(3)
+        tree.insert(4)
+        tree.print_tree()
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [2, 1, 3, 4])
+        self.assertEqual(tree_preorder_color, ['black', 'black', 'black', 'red'])
+        print("\n")
+
+
+    def test_tree_insert_color_1(self):
+        print("\n")
+        print("tree_color_check")
+        
+        tree = rb_tree()
+
+        for i in range(1, 8):
+            tree.insert(i)
+        tree.print_tree()
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [2, 1, 4, 3, 6, 5, 7])
+        self.assertEqual(tree_preorder_color, ['black', 'black', 'red', 'black', 'black', 'red', 'red'])
+        print("\n")
+
+class T3_tree_delete(unittest.TestCase):
+
+
+    def test_tree_delete_0(self):
+        print("\n")
+        print("tree_insert")
+        #print("checking in order, preorder and post order")
+        tree = rb_tree()
+
+        tree.insert(7)
+        tree.insert(5)
+        tree.insert(9)
+        tree.insert(6)
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [7, 5, 6, 9])
+        self.assertEqual(tree_preorder_color, ['black', 'black', 'red', 'black'])
+        tree.delete(9)
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [6, 5, 7])
+        self.assertEqual(tree_preorder_color, ['black', 'black', 'black'])
+        print("\n")
+
+    def test_tree_delete_1(self):
+        print("\n")
+        print("tree_insert")
+        print("checking in order, preorder and post order")
+        tree = rb_tree()
+
+        for i in range(1, 8):
+            tree.insert(i)
+        tree.delete(5)
+        tree.delete(4)
+        # tree.print_tree()
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [2, 1, 6, 3, 7])
+        self.assertEqual(tree_preorder_color, ['black', 'black', 'red', 'black', 'black'])
+        print("\n")
+    
+    def test_tree_delete_color_2(self):
+        print("\n")
+        print("tree_left_rotation")
+        tree = rb_tree()
+        tree.insert(7)
+        tree.insert(5)
+        tree.insert(9)
+        tree.insert(3)
+        tree.insert(6)
+        tree.insert(8)
+        tree.insert(10)
+        tree.insert(1)
+        tree.insert(2)
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [7, 5, 2, 1, 3, 6, 9, 8, 10])
+        self.assertEqual(tree_preorder_color, ['black', 'red', 'black', 'red', 'red', 'black', 'black', 'red', 'red'])
+        tree.delete(6)
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [7, 2, 1, 5, 3, 9, 8, 10])
+        self.assertEqual(tree_preorder_color, ['black', 'red', 'black', 'black', 'red', 'black', 'red', 'red'])
+        tree.delete(7)
+        tree_preorder = [node.data for node in tree.preorder()]
+        tree_preorder_color = [node.color for node in tree.preorder()]
+        self.assertEqual(tree_preorder, [8, 2, 1, 5, 3, 9, 10])
+        self.assertEqual(tree_preorder_color, ['black', 'red', 'black', 'black', 'red', 'black', 'red'])
+        print("\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
